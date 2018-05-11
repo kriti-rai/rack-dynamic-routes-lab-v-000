fc9ab9fc8f8b79ece3ee50c7b6e3f31112cf6743
Kriti Rai <kriti.rai@gmail.com>
2018-05-11 03:46:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,9 +12,11 @@ class Application
       else
         resp.status = 400
         resp.write "Item not found"
+      end
     else
       resp.status = 404
       resp.write "Route not found"
+    end
 
     resp.finish
   end
