0503e141b4cd83609c3c788ddfa3e0c3d896a6ac
Kriti Rai <kriti.rai@gmail.com>
2018-05-11 03:46:32 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,22 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      search_item = req.params["item"]
+      if @@items.include?(item)
+        resp.status = 200
+        resp.write "#{item}, #{item.price}"
+      else
+        resp.status = 400
+        resp.write "Item not found"
+    else
+      resp.status = 404
+      resp.write "Route not found"
+
+    resp.finish
+  end
+
+end
